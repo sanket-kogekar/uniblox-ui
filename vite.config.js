6740a1a91009d0f9296c3ad01ae2271a9c8edcaa
sanket-kogekar <75581935+sanket-kogekar@users.noreply.github.com>
2025-08-05 12:32:33 +0530

    backend URL added

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://uniblox-server-25v0.onrender.com',
         changeOrigin: true,
       },
     },
